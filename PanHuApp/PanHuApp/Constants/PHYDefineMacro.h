bc1e437a5c0fba5312beb6ec9c60acdcf8060f65
yyz <yyzidcards.sina.com>
2016-06-10 17:24:13 +0800

    0610.2

--- PanHuApp/PanHuApp/Constants/PHYDefineMacro.h
+++ PanHuApp/PanHuApp/Constants/PHYDefineMacro.h
@@ -6,7 +6,14 @@
 //  Copyright © 2016年 戴维营学习. All rights reserved.
 //
 
+/*!
+ *  主屏范围及其宽、高
+ */
 #define PHYScreenBounds [UIScreen mainScreen].bounds
 #define PHYScreenH [UIScreen mainScreen].bounds.size.height
 #define PHYScreenW [UIScreen mainScreen].bounds.size.width
 
+/*!
+ *  Buy页面【分类】item 宽度
+ */
+#define BuyCategaryItemWidth
